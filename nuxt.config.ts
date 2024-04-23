f0db99703b83e430f39e86fec283a4a15a8d13b6
Raj Saroj <raj.s.saroj@gmail.com>
2024-04-23 12:47:18 +0530

    added nuxt-oidc-auth as global middleware

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@vueuse/nuxt", "nuxt-icon"],
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+    "nuxt-oidc-auth",
+  ],
+
+  routeRules: {
+    "/backendapi/**": {
+      proxy: {
+        to: `${process.env.NUXT_APP_BACKEND_URL}/api/**`,
+      },
+    },
+    "/hardlogout": {
+      redirect: `${process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_BASE_URL}/protocol/openid-connect/logout?post_logout_redirect_uri=${process.env.NUXT_APP_DOMAIN}&client_id=${process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_CLIENT_ID}`,
+    },
+  },
 
   tailwindcss: {
     exposeConfig: true,
@@ -13,6 +31,23 @@ export default defineNuxtConfig({
 
   typescript: {
     shim: false,
+  },
+
+  oidc: {
+    defaultProvider: "keycloak",
+    providers: {
+      keycloak: {
+        baseUrl: process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_BASE_URL as string,
+        clientId: process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_CLIENT_ID as string,
+        clientSecret: process.env.NUXT_OIDC_PROVIDERS_KEYCLOAK_CLIENT_SECRET as string,
+        redirectUri: "http://localhost:3000/auth/keycloak/callback",
+        exposeAccessToken: true,
+      },
+    },
+    middleware: {
+      globalMiddlewareEnabled: true,
+      customLoginPage: false,
+    },
   },
 
   imports: {
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Index Page</h1>
+  <h3>user: {{ user }}</h3>
+</template>
+<script setup>
+  const { loggedIn, user, refresh, login, logout, currentProvider, clear } = useOidcAuth();
+</script>
