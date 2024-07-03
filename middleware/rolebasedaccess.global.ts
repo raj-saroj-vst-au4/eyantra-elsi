e0286911100af9a4df8d7e77c4210b84b099e22c
Raj Saroj <raj.s.saroj@gmail.com>
2024-07-03 12:14:01 +0530

    check user token before rbac

--- middleware/rolebasedaccess.global.ts
+++ middleware/rolebasedaccess.global.ts
@@ -3,25 +3,26 @@ import { jwtDecode } from "jwt-decode";
 export default defineNuxtRouteMiddleware((to, next) => {
   // skip middleware on server
   // if (import.meta.server) return;
-  const { user } = useOidcAuth();
+
   const checkRole = () => {
+    const { user } = useOidcAuth();
     const rawjwt = user.value?.accessToken;
+    if (!rawjwt) return false;
     const decodedToken = jwtDecode(rawjwt);
     return decodedToken?.realm_access?.roles;
   };
 
   if (
-    user.value?.accessToken &&
-    (to.path == "/addlabslot" ||
-      to.path == "/labinaug" ||
-      to.path == "/colleges" ||
-      to.path == "/createworkshop" ||
-      to.path == "/users" ||
-      to.path == "/addlabslot" ||
-      to.path == "/admins")
+    to.path == "/addlabslot" ||
+    to.path == "/labinaug" ||
+    to.path == "/colleges" ||
+    to.path == "/createworkshop" ||
+    to.path == "/users" ||
+    to.path == "/addlabslot" ||
+    to.path == "/admins"
   ) {
     const jwtroles = checkRole();
-    if (jwtroles?.includes("eyantra_admin")) {
+    if (jwtroles && jwtroles?.includes("eyantra_admin")) {
       return;
     } else {
       return navigateTo("/");
